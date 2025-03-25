5c20d3f4751fcd5f5ac63751dcdecc512b271556
smwalke83 <smwalk83@gmail.com>
2025-03-24 18:32:40 -0600

    copy to public working

--- src/main.py
+++ src/main.py
@@ -5,23 +5,25 @@ import os
 import shutil
 
 def main():
-    node = TextNode("My Text", "My Text Type", "https://www.fakeurl.com")
-    print(node)
+    copy_to_public("static", "public")
 
 
-def copy_to_public(source):
-    shutil.rmtree("public")
-    os.mkdir("public")
-    new_filepath = "public"
-    if os.path.isfile(source):
-        shutil.copy(source, new_filepath)
-    else:
-        contents = listdir(source)
-        for item in contents:
-            os.path.join(new_filepath, item)
-            copy_to_public(item)
+def copy_to_public(source, destination):
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+    if not os.path.exists(destination):
+        os.mkdir(destination)
+    items = os.listdir(source)
+    for item in items:
+        source_path = os.path.join(source, item)
+        dest_path = os.path.join(destination, item)
+        if os.path.isfile(source_path):
+            shutil.copy(source_path, dest_path)
+        else:
+            os.mkdir(dest_path)
+            copy_to_public(source_path, dest_path)
     
 
-    # This is untested, but clearly it cannot delete the public directory every loop.
+
     
 main()
